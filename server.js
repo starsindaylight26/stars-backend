a6d14cb3248bf05db0b8931b18736b9381901611
starsindaylight26 <starsindaylight26@gmail.com>
2026-05-08 18:54:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const db = mysql.createPool({
   port:     process.env.DB_PORT,
   database: process.env.DB_NAME,
   user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10
 });
