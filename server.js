13330373335ac009fb4619b4ccdd1a094e561bea
starsindaylight26 <starsindaylight26@gmail.com>
2026-05-08 18:47:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -50,11 +50,11 @@ const upload = multer({
 
 // ---- DATABASE ----
 const db = mysql.createPool({
-  host:     'localhost',
-  port:     3306,
-  user:     'root',
-  password: '',
-  database: 'stars_db_new',
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD
   waitForConnections: true,
   connectionLimit: 10
 });
